76954c09240a0188ab778b6a5091e3427a955e94
Justin Goh <radium.laboratories@gmail.com>
2019-11-30 12:46:25 +0800

    init xcode project

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by JUSTIN GOH on 30/11/19.
+//  Copyright © 2019 Radium Laboratories. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
